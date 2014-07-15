d44067c31a6e6f9344095c753bd76f1577e089b8
Dirk Sierd de Vries <dirksierd@me.com>
2014-07-15 19:36:57 +0200

    rubocop check

--- lib/omniauth/strategies/dirk.rb
+++ lib/omniauth/strategies/dirk.rb
@@ -3,20 +3,17 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Dirk < OmniAuth::Strategies::OAuth2
-      # change the class name and the :name option to match your application name
       option :name, :dirk
 
-      option :client_options, {
-        :site => "http://dirk.dev",
-        :authorize_url => "/oauth/authorize"
-      }
+      option :client_options,
+             site: 'http://dirk.dev',
+             authorize_url: '/oauth/authorize'
 
       uid { raw_info['id'] }
 
       info do
         {
-          :email_address => raw_info['email_address']
-          # and anything else you want to return to your API consumers
+          email_address: raw_info['email_address']
         }
       end
 
