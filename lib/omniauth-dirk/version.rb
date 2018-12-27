187c0c724e3242d20ac03bb2cc3ced8fa108e116
Dirk Sierd de Vries <dirksierd@me.com>
2018-12-27 22:38:00 +0100

    that was dumb

--- lib/omniauth-dirk/version.rb
+++ lib/omniauth-dirk/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Dirk
-    VERSION = '1.0.6'.freeze
+    VERSION = '1.0.7'.freeze
   end
 end
--- lib/omniauth/strategies/dirk.rb
+++ lib/omniauth/strategies/dirk.rb
@@ -6,7 +6,7 @@ module OmniAuth
       option :name, :dirk
 
       option :client_options,
-             site: 'https://dirk.test',
+             site: 'https://dirk.cda.nl',
              authorize_url: '/oauth/authorize'
 
       uid { raw_info['id'] }
